88e6919eefacdadcb65412251d316c00202251ac
bartavelle <bartavelle@gmail.com>
2015-09-24 10:19:47 +0200

    Disable stack explosion test by default

--- tests/spaceleak.hs
+++ tests/spaceleak.hs
@@ -3,6 +3,7 @@ module Main where
 
 import qualified Control.Monad.Trans.RSS.Lazy as RSSL
 import qualified Control.Monad.Trans.RSS.Strict as RSSS
+import System.Environment
 import Control.Monad.Writer
 
 n :: Int
@@ -11,6 +12,11 @@ n = 10000000
 main :: IO ()
 main = do
     print $ RSSS.runRSS (replicateM_ n $ tell $ Sum (1 :: Int)) () ()
-    putStrLn "Strict version ok, the next test should explode the stack."
-    print $ RSSL.runRSS (replicateM_ n $ tell $ Sum (1 :: Int)) () ()
-    putStrLn "Lazy version should have exploded !"
+    tryExplode <- fmap (not . null) getArgs
+    if tryExplode
+        then do
+            putStrLn "Strict version ok, the next test should explode the stack."
+            print $ RSSL.runRSS (replicateM_ n $ tell $ Sum (1 :: Int)) () ()
+            putStrLn "Lazy version should have exploded !"
+        else
+            putStrLn "Do not try exploding the stack. Run the test program with any command line argument to test it"
